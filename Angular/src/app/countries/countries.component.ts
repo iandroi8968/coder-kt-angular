4c07c49bc59c15571b4ccaa9be9e598ee0a76741
Irshad Alam <iandroid8968@gmail.com>
2018-07-19 01:05:17 +0530

    added mat spinner while table is loading

--- Angular/src/app/countries/countries.component.ts
+++ Angular/src/app/countries/countries.component.ts
@@ -11,6 +11,7 @@ import { MatTableDataSource, MatPaginator } from '../../../node_modules/@angular
 export class CoursesComponent implements OnInit {
   private result : Result[];
   private dataSource : MatTableDataSource<Result>;
+  private hide : boolean;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   displayedColumns = ['name', 'alpha2_code', 'alpha3_code'];
   
@@ -18,10 +19,12 @@ export class CoursesComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.countriesService.getCountries().subscribe((data : RestResponse[]) {
+    this.hide = true;
+    this.countriesService.getCountries().subscribe((data : RestResponse[]) => {
       this.result = data['RestResponse']['result'];
       this.dataSource = new MatTableDataSource<Result>(this.result);
       this.dataSource.paginator = this.paginator;
+      this.hide = false;
     });
   }
 }
